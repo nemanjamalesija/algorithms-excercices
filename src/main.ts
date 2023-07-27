31fe8b8acca08ec4594d22ebba88068bd29424a1
nemanjamalesija <malesija13@gmail.com>
2023-07-27 03:30:41 +0200

    Add two sum

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,29 @@ function quickSort(array: number[]): number[] {
   return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
 }
 
-console.log(quickSort(array));
+// console.log(quickSort(array));
+
+// TWO SUM
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+const nums = [3, 3];
+const target = 6;
+
+function twoSums(array: number[], target: number) {
+  for (let i = 0; i <= array.length - 1; i++) {
+    for (let j = i + 1; j <= array.length - 1; j++) {
+      if (array[i] + array[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+// console.log(twoSums(nums, target));
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-
   </div>
 `;
