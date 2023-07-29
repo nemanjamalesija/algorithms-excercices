3dda694dd639c1a37a2636a9d015901ea4960b14
nemanjamalesija <malesija13@gmail.com>
2023-07-29 22:37:59 +0200

    Add buddy string

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css';
 
 // QUICK SORT
-const array = [8, 3, 1, 5, 9, 2, 7, 4, 6];
 
 function quickSort(array: number[]): number[] {
   if (array.length <= 1) {
@@ -32,7 +31,7 @@ function quickSortares(array: number[]): number[] {
   return [...quickSortares(left), pivot, ...quickSort(right)];
 }
 
-console.log(quickSortares([2, 1, 4, 5, 9, 22, 11, 6, 55, 3]));
+// console.log(quickSortares([2, 1, 4, 5, 9, 22, 11, 6, 55, 3]));
 
 // console.log(quickSort(array));
 
@@ -92,7 +91,6 @@ function isValid(s: string) {
 
     if (char === '(' || char === '[' || char === '{') {
       stack.push(char);
-      console.log(stack);
     } else {
       const lastOpening = stack.pop();
 
@@ -280,6 +278,98 @@ function minArraySum(nums: number[], target: number) {
 }
 
 // console.log(minArraySum([2, 3, 1, 2, 4, 3], 3));
+
+// function mergeSort(array) {
+//   if (array.length <= 1) {
+//     return array; // Base case: array with 0 or 1 element is already sorted
+//   }
+
+//   // Split the array into two halves
+//   const middle = Math.floor(array.length / 2);
+//   const leftHalf = array.slice(0, middle);
+//   const rightHalf = array.slice(middle);
+
+//   // Recursively sort each half
+//   const sortedLeft = mergeSort(leftHalf);
+//   const sortedRight = mergeSort(rightHalf);
+
+//   // Merge the sorted halves
+//   return merge(sortedLeft, sortedRight);
+// }
+
+// function merge(left, right) {
+//   const mergedArray = [];
+//   let leftIndex = 0;
+//   let rightIndex = 0;
+
+//   // Merge the two sorted arrays into a single sorted array
+//   while (leftIndex < left.length && rightIndex < right.length) {
+//     if (left[leftIndex] < right[rightIndex]) {
+//       mergedArray.push(left[leftIndex]);
+//       leftIndex++;
+//     } else {
+//       mergedArray.push(right[rightIndex]);
+//       rightIndex++;
+//     }
+//   }
+
+//   // Add any remaining elements from the left and right arrays
+//   while (leftIndex < left.length) {
+//     mergedArray.push(left[leftIndex]);
+//     leftIndex++;
+//   }
+
+//   while (rightIndex < right.length) {
+//     mergedArray.push(right[rightIndex]);
+//     rightIndex++;
+//   }
+
+//   return mergedArray;
+// }
+
+// // Example usage:
+// const arr = [38, 27, 43, 3, 9, 82, 10];
+// const sortedArr = mergeSort(arr);
+// console.log('Original array:', arr);
+// console.log('Sorted array:', sortedArr);
+
+// BUDDY STRINGS
+// Input: s = "ab", goal = "ba"
+// Output: true
+// Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+
+function buddyStrings(s: string, goal: string) {
+  if (s.length !== goal.length || s.length <= 1) return false;
+  if (s === goal) return new Set(s).size < s.length;
+
+  let buddy = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== goal[i]) buddy.push(i);
+  }
+
+  if (buddy.length !== 2) return false;
+  if (s[buddy[0]] !== goal[buddy[1]] || s[buddy[1]] !== goal[buddy[0]])
+    return false;
+
+  return true;
+}
+
+console.log(buddyStrings('aaaaaaabc', 'aaaaaaacb'));
+
+// aaaaaaba c
+
+/* 
+i + 1
+a => a
+a => a
+a => a
+a => a
+a => a
+a => a
+
+
+
+*/
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
