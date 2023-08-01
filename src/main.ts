f1b82ae31c068bf77af12aa6040bf632f7b52807
nemanjamalesija <malesija13@gmail.com>
2023-08-01 22:25:32 +0200

    Add factorial recursion

--- src/main.ts
+++ src/main.ts
@@ -568,14 +568,15 @@ function nestedAdd(input: any) {
   return sum;
 }
 
-console.log(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11]));
+// console.log(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11]));
 
-// test.skip('nested arrays addition', () => {
-//   expect(nestedAdd([1, 2, 3])).toEqual(6);
-//   expect(nestedAdd([1, [2], 3])).toEqual(6);
-//   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
-//   expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
-// });
+function factorialRecursion(n: number): number {
+  if (n === 1) return n * 1;
+
+  return n * factorialRecursion(n - 1);
+}
+
+console.log(factorialRecursion(5));
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
