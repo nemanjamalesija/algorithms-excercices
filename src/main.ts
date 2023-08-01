7589a8e07c42ede5288aaaff1281d34c2a374481
nemanjamalesija <malesija13@gmail.com>
2023-08-01 21:16:36 +0200

    Add fibonacci loop

--- src/main.ts
+++ src/main.ts
@@ -540,7 +540,19 @@ function fibonacci(n: number): number {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(10));
+// console.log(fibonacci(3));
+
+function fibonacciLoop(n: number) {
+  const sequence = [0, 1];
+
+  for (let i = 2; i < n + 1; i++) {
+    sequence.push(sequence[i - 2] + sequence[i - 1]);
+  }
+
+  return sequence[n];
+}
+
+console.log(fibonacciLoop(10));
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
