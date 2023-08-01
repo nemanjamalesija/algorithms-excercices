8ead8060e041dc7095593f52ca5905f50595fe34
nemanjamalesija <malesija13@gmail.com>
2023-08-01 20:13:22 +0200

    Add insertion sort

--- src/main.ts
+++ src/main.ts
@@ -497,7 +497,41 @@ function groupAnagrams(strs: string[]) {
   return Array.from(map.values());
 }
 
-console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+// console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+
+function insertionSort(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > current; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = current;
+  }
+
+  return arr;
+}
+
+// console.log(insertionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
+
+function insertionSortWhile(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let current = arr[i];
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+
+      j--;
+    }
+    arr[j + 1] = current;
+    j--;
+  }
+  return arr;
+}
+
+console.log(insertionSortWhile([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
